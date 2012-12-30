d9089c1192865b1ac74fa1d2b9ffbd02660cb7ec
Russ Adams <russa@arcatechsystems.com>
2012-12-30 14:53:12 -0500

    Adding Story summary.

--- story.php
+++ story.php
@@ -3,44 +3,53 @@
 
 <div id="title"><img src="images/story.gif" /></div>
 
-<!--
-	Hand:
-	I don't want to go.
-	
-	Sulah:
-	It is what young men your age do.
-	
-	Hand:
-	But it's not what I do.
-	
-	Sulah:
-	The way you speak, it almost seems as though you have a choice.</span>
-	
-	Hand:
-	You're impossible.
-	
-	Sulah:
-	I am.
--->
-
-<br />
-<h2>Story Under Development</h2>
-<p>Help define our gameplay and story.  We have an exciting <a href="http://wiki.asgardrpg.org/index.php?title=Story">story</a> unrolling on the <a href="http://wiki.asgardrpg.org/">wiki</a>.</p>
 <img class="img-right" src="images/roadmap-thmb.jpg" />
 <br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
+<h3>Hand:</h3>
+<p>I don't want to go.</p>
+	
+<h3>Sulah:</h3>
+<p>It is what young men your age do.</p>
+	
+<h3>Hand:</h3>
+<p>But it's not what I do.</p>
+	
+<h3>Sulah:</h3>
+<p>The way you speak, it almost seems as though you have a choice.</p>
+	
+<h3>Hand:</h3>
+<p>You're impossible.</p>
+	
+<h3>Sulah:</h3>
+<p>I am.</p>
+
+<h3>Synopsis</h3>
+<p>Long ago, a rotten man named Vear brought our world of Brant into an age of
+despair and ignorance.  At the height of a series of horrible attacks, a
+fearless woman and her children were able to seal Vear into the Void.  But the
+damage had been done: our sophisticated culture was brought to its knees.</p>
+
+<p>Our stories, our history, our homelands were forever lost.  Vear took it all
+away.  Now ages have come and gone and the seal has erroded.  Vear's madness
+discreetly touches all aspects of our society.</p>
+
+<p>The last of the Skraelingar attack supply caravans.  Markland and Helluland
+are at war.  Terrible creatures emerge from water and caves and descend from
+above the clouds.</p>
+
 <br />
 <br />
 
-<h2>Questions?</h2>
-<p>Please email <a href="mailto:asgard.team@gmail.com">asgard.team@gmail.com</a> with any questions you may have.</p>
+<h3>Want to contribute?</h3>
+<p>The Asgard Team maintains a skunkworks wiki for the story and gameplay.
+We'd love your help in this endevour.  Please shoot us an email at <a
+href="mailto:asgard.team@gmail.com">asgard.team@gmail.com</a>.</p>
+
+<br />
+<br />
+<br />
+<br />
+<br />
 
 <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 
